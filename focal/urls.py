df2316bfcff692ac0716080892e89e3cd51a9a6c
VinCoD <odipovincent161@gmail.com>
2021-11-15 15:59:03 +0300

    Added some meaningful changes to the program.

--- focal/urls.py
+++ focal/urls.py
@@ -5,4 +5,5 @@ app_name = 'focal'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('missions', views.missions, name='missions' ),
 ]
--- focal/views.py
+++ focal/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Mission
 
 # Create your views here.
 
 def home(request):
     return render(request, 'focal/home.html')
+
+
+def missions(request):
+    missions = Mission.objects.order_by('date_added')
+    context = {'missions': missions}
+    return render(request, 'focal/missions.html', context)
