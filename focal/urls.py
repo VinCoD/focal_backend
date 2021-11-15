2c58ef228df36c5cef72c36bb4b3cceeb84c8a82
VinCoD <odipovincent161@gmail.com>
2021-11-15 17:06:49 +0300

    Can now allow users to add their own missions

--- focal/urls.py
+++ focal/urls.py
@@ -6,4 +6,5 @@ app_name = 'focal'
 urlpatterns = [
     path('', views.home, name='home'),
     path('missions', views.missions, name='missions' ),
+    path('new_mission', views.new_mission, name="new_mission"),
 ]
--- focal/views.py
+++ focal/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Mission
+from .forms import MissionForm
 
 # Create your views here.
 
@@ -11,3 +12,16 @@ def missions(request):
     missions = Mission.objects.order_by('date_added')
     context = {'missions': missions}
     return render(request, 'focal/missions.html', context)
+
+def new_mission(request):
+    if request.method != 'POST':
+        form = MissionForm()
+    else:
+        form = MissionForm(data=request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('focal:missions')
+    
+    context = {'form': form}
+    return render(request, 'focal/new_mission.html', context)
+
